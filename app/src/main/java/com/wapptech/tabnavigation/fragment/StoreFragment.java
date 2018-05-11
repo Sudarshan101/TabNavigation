3e529ae7eff831e00c680ae1be2d73e72bb2789b
Sudarshan101 <sudarshan.vis101@gmail.com.com>
2018-05-11 12:07:08 +0530

    source code

---
+++ app/src/main/java/com/wapptech/tabnavigation/fragment/StoreFragment.java
@@ -0,0 +1,45 @@
+package com.wapptech.tabnavigation.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wapptech.tabnavigation.R;
+
+public class StoreFragment extends Fragment {
+
+    private static final String TAG = StoreFragment.class.getSimpleName();
+
+
+    private RecyclerView recyclerView;
+
+    public StoreFragment() {
+        // Required empty public constructor
+    }
+
+    public static StoreFragment newInstance(String param1, String param2) {
+        StoreFragment fragment = new StoreFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_store, container, false);
+        return view;
+    }
+
+
+}
